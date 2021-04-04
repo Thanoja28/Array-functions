40c1bcb5ec9dcbd653fe851780eb628e4e79b2d1
Thanoja Thangaraja <thanujathanuja18@yahoo.com>
2021-04-04 21:25:46 +0100

    sorting is done

--- source.js
+++ source.js
@@ -6,6 +6,7 @@ const employees = [ { first: "Thanoja", last: "Thangaraja", salary: 400 },
                     { first: "Royal", last: "veeman", salary: 5000},
                     ]
 
+
 // filter to get info on the persons with salary more than £2000.
 const upsalary = employees.filter(employee => {
   if(employee.salary >= 2000) {
@@ -43,7 +44,21 @@ console.table(employeeSalary);
 
 // https://en.wikipedia.org/wiki/List_of_areas_of_London - sorting cities in london
 
-const cities = document.querySelector('table tbody tr td');
+const cities = document.querySelector('.vector-menu-content');
 const links = Array.from(cities.querySelectorAll('a'));
 
-const en = links.map(link => link.textContent);
+const en = links.map(link => link.textContent)
+                .filter(name => name.includes('en'));
+
+// -----------------------------------------------------------------
+
+const persons = [ "Alan, William", "Vishu, Henry", "Sita, Ram", "Nehru, Hoise", "Cindrella, Noel", "Thanu, Rasan", "Giri, Whale", "Gajini, Nimal" ]
+// sort last names alphabetically
+
+const sortAlpha = persons.sort((firstone, nextone) => {
+  const [aFirst, aLast] = firstone.split(', ');
+  const [bFirst, bLast] = nextone.split(', ');
+  return aLast > bLast ? 1 : -1;
+  console.log(sortAlpha);
+});
+
